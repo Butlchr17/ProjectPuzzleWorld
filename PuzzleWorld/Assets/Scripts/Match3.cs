4f36e38fe5f43ebd1b92799e80cd22a6b8e96c51
Butlchr17 <butlchr17@gmail.com>
2025-09-29 04:21:01 -0700

    C) Added 6 different orb temp sprites, updated some of the game logic in Match3

--- PuzzleWorld/Assets/Scripts/Match3.cs
+++ PuzzleWorld/Assets/Scripts/Match3.cs
@@ -10,11 +10,47 @@ namespace PuzzleWorld
         [SerializeField] Vector3 originPosition = Vector3.zero;
         [SerializeField] bool debug = true;
 
+        [SerializeField] Orb orbPrefab;
+        [SerializeField] OrbType[] orbTypes;
+
         GridSystem2D<GridObject<Orb>> grid;
 
         void Start()
         {
-            grid = GridSystem2D<GridObject<Orb>>.VerticalGrid(width, height, cellSize, originPosition, debug);
+           InitializeGrid();
         }
+
+        void InitializeGrid()
+        { 
+            grid = GridSystem2D<GridObject<Orb>>.VerticalGrid(width, height, cellSize, originPosition, debug);
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    CreateOrb(x, y);
+                }
+            }
+        }
+
+        void CreateOrb(int x, int y)
+        {
+            Orb orb = Instantiate(orbPrefab, grid.GetWorldPositionCenter(x, y), Quaternion.identity, transform);
+            orb.SetType(orbTypes[Random.Range(0, orbTypes.Length)]);
+            var gridObject = new GridObject<Orb>(grid, x, y);
+            gridObject.SetValue(orb);
+            grid.SetValue(x, y, gridObject);
+        }
+
+        // Init grid
+
+        // Read player input and swap orbs
+
+        // Start coroutine:
+        // Swap animation
+        // Check matches
+        // Make Orbs Explode
+        // Make Orbs fall down
+        // Fill empty spaces with new orbs
+        // Is game over?
     }
 }
